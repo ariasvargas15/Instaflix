5e4b52686332321ad024d5357863a0100f13ef04
Brayan Arias <stevenson.arias@gmail.com>
2022-06-22 10:51:37 -0500

    Add skeleton (#5)
    * Add skeleton

--- core/src/main/java/com/bsav/core/presentation/glide/GlideHelper.kt
+++ core/src/main/java/com/bsav/core/presentation/glide/GlideHelper.kt
@@ -1,13 +1,28 @@
 package com.bsav.core.presentation.glide
 
+import android.graphics.Bitmap
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 
 private const val BASE_URL_IMAGES = "https://image.tmdb.org/t/p/original"
 
 fun ImageView.loadImageFromUrl(url: String) {
+    val requestOptions: RequestOptions = RequestOptions()
+        .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+        .skipMemoryCache(true)
+        .centerCrop()
+        .dontAnimate()
+        .dontTransform()
+        .priority(Priority.IMMEDIATE)
+        .encodeFormat(Bitmap.CompressFormat.PNG)
+        .format(DecodeFormat.DEFAULT)
     Glide.with(this)
+        .applyDefaultRequestOptions(requestOptions)
         .load(url)
         .thumbnail(0.05f)
         .transition(DrawableTransitionOptions.withCrossFade())
